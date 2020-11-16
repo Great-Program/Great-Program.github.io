5acdbd5613b216bbf22113efec8dda3a425d10f4
Cloud-HTML <74518409+Cloud-HTML@users.noreply.github.com>
2020-11-16 09:51:21 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<link rel="stylesheet" href="/css/style.css" />
+<title>Great Program</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=screen-width,inital-scale=1.0" />
+</head>
+<body>
+<h1>Great-Program.github.io</h1>
+<?php echo "Helping each other with code." ?>
+<script src="/js/script.js"></script>
+</body>
+</html>
